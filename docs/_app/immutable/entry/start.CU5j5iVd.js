0f7762ad95afdfd151c231aa4a25c8f441905c14
Giovanni Iacuzzo <giovanniiacuzzo@Giovannis-MacBook-Pro.local>
2024-05-10 12:00:31 +0200

    build

---
+++ docs/_app/immutable/entry/start.CU5j5iVd.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.dWacxe16.js";export{t as start};
--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,12 +1,12 @@
 <script>
     import Button from "./Button.svelte";
 </script>
-<header class="max-w-xl mx-auto flex flex-col items-center py-8">
+<header class="max-w-xl mx-auto flex flex-col items-center py-8 px-4">
     <img src="./avatar.jpg" alt="Giovanni Iacuzzo avatar" width="40" height="40" class="rounded-full mb-4">
     <h1 class="text-base font-medium tracking-tight">Hey, I’m Giovanni Iacuzzo</h1>
     <p class="text-base tracking-tight">Product Designer</p>
 </header>
-<section class="max-w-xl mx-auto tagline flex flex-col items-center pb-8">
+<section class="max-w-xl mx-auto tagline flex flex-col items-center pb-8 px-4">
     <h2 class="md:text-5xl text-4xl text-center mb-4 tracking-tighter font-regular leading-big-title md:leading-big-title">I help businesses create beautiful and <span class="text-nowrap">easy-to-use</span> digital products <span class="text-violet-500">ツ</span></h2>
     <p class="text-lg text-center mb-8 px-10 tracking-tight text-stone-600">Over a decade of experience designing websites, mobile apps, software interface, icons and more</p>
     <Button>Get in touch</Button>
