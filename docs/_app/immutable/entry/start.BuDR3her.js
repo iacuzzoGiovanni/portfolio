43df473965e0e7b97ecff319bd922187031ceb40
Giovanni <iacuzzogiovanni@gmail.com>
2024-05-10 22:21:13 +0200

    build

---
+++ docs/_app/immutable/entry/start.BuDR3her.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CKmr2Qhd.js";export{t as start};
--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,13 +3,13 @@
 </script>
 <footer class="max-w-screen-sm mx-auto selected-work pt-8 px-4">
     <h3 class="text-3xl text-center mb-2 tracking-tight mb-8">Made with ❤️</h3>
-    <div class="flex justify-center mb-8">
-        <ul class="flex-col md:flex-row flex gap-y-8 md:gap-x-8 justify-center items-center">
-            <li><Button>Get in touch</Button></li>
+    <div class="mb-8 text-center sm:flex sm:justify-center">
+        <Button>Get in touch</Button>
+        <ul class="w-full sm:w-auto inline-flex gap-x-8 justify-center sm:items-center sm:ml-8 mt-8 sm:mt-0">
             <li><a href="https://twitter.com/IacuzzoG" target="_blank" class="text-base underline underline-offset-4 font-regular hover:text-stone-700">Twitter</a></li>
             <li><a href="https://dribbble.com/iacuzzogiovanni" target="_blank" class="text-base underline underline-offset-4 font-regular hover:text-stone-700">Dribbble</a></li>
             <li><a href="https://linkedin.com/in/giovanni-iacuzzo-15163959" target="_blank" class="text-base underline underline-offset-4 font-regular hover:text-stone-700">Linkedin</a></li>
         </ul>
     </div>
-    <p class="text-center text-5xl md:text-7xl tracking-tighter overflow-hidden"><span class="relative top-4 md:top-6">Giovanni Iacuzzo</span></p>
+    <p class="text-center text-[2.75rem] sm:text-7xl tracking-tighter overflow-hidden"><span class="relative top-6">Giovanni Iacuzzo</span></p>
 </footer>
