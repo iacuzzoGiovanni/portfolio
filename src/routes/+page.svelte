d0267e96619ec58fec4a249cd081075109af327d
Giovanni <iacuzzogiovanni@gmail.com>
2024-05-17 15:22:55 +0200

    build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,6 @@
 <svelte:head>
 	<title>Giovanni Iacuzzo | Product Designer helping businesses create user-friendly digital products</title>
 	<meta name="description" content="Giovanni Iacuzzo, Product Designer with over a decade of experience in designing websites, mobile apps, software interfaces, icons, and more. Creating beautiful, user-friendly digital products." />
-  <meta name="keywords" content="Product Designer, UX/UI Designer, Digital Product Design, Web Design, Mobile App Design, Software Interface Design, Icon Design, User-Friendly Design, Digital Products, Giovanni Iacuzzo, Experienced Designer, Design Portfolio, Freelance Designer, Visual Designer, Creative Designer">
-  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
-  <link rel="manifest" href="./site.webmanifest">
-  <link rel="mask-icon" href="./safari-pinned-tab.svg" color="#5bbad5">
-  <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="theme-color" content="#ffffff">
   <!-- Person Schema -->
   <script type="application/ld+json">
   {
@@ -34,6 +26,14 @@
     "name": "Giovanni Iacuzzo | Product Designer helping businesses create user-friendly digital products."
   }
   </script>
+  <meta name="keywords" content="Product Designer, UX/UI Designer, Digital Product Design, Web Design, Mobile App Design, Software Interface Design, Icon Design, User-Friendly Design, Digital Products, Giovanni Iacuzzo, Experienced Designer, Design Portfolio, Freelance Designer, Visual Designer, Creative Designer">
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+  <link rel="manifest" href="./site.webmanifest">
+  <link rel="mask-icon" href="./safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
 </svelte:head>
 
 <script>
